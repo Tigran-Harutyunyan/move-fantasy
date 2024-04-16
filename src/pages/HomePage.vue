e4c73f26e5b4458210eb97fbab3ea422bf0d81d7
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-16 23:39:08 +0400

    finished Home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HeroBanner from "@cmp/home/HeroBanner.vue";
+import Trending from "@cmp/home/Trending.vue";
+import Popular from "@cmp/home/Popular.vue";
+import TopRated from "@cmp/home/TopRated.vue";
+</script>
+
+<template>
+  <div class="homePage">
+    <HeroBanner />
+    <Trending />
+    <Popular />
+    <TopRated />
+  </div>
+</template>
