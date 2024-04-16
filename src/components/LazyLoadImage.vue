a88c973a7c919f0042afe4ffcafc8cbc4ebe6a41
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-16 23:38:23 +0400

    lazyload component

---
+++ src/components/LazyLoadImage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { toRefs } from "vue";
+
+type Props = {
+  src: string;
+  className?: string;
+};
+const props = defineProps<Props>();
+
+const { src, className } = toRefs(props);
+</script>
+
+<template>
+  <img v-lazy="{ src: src }" />
+</template>
