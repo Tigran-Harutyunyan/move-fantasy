4edfa331733b60e0dd57042342c5a8d7f7f08a93
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-16 13:09:11 +0400

    initialized application

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@assets",
+        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
+      },
+      {
+        find: "@cmp",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      },
+      {
+        find: "@stores",
+        replacement: fileURLToPath(new URL("./src/stores", import.meta.url)),
+      },
+      {
+        find: "@use",
+        replacement: fileURLToPath(
+          new URL("./src/composables", import.meta.url)
+        ),
+      },
+      {
+        find: "@types",
+        replacement: fileURLToPath(new URL("./src/types", import.meta.url)),
+      },
+    ],
+  },
+});
