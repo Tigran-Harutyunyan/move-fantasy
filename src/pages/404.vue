317870488cea44c1e656a331acfb321f9eb13e80
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-19 13:11:42 +0400

    added 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import ContentWrapper from "../components/ContentWrapper.vue";
+</script>
+
+<template>
+  <div class="pageNotFound">
+    <ContentWrapper>
+      <span class="bigText">404</span>
+      <span class="smallText">Page not found!</span>
+    </ContentWrapper>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.pageNotFound {
+  height: 700px;
+  padding-top: 200px;
+  .contentWrapper {
+    text-align: center;
+    color: var(--black-light);
+    display: flex;
+    flex-direction: column;
+    .bigText {
+      font-size: 150px;
+      font-weight: 700;
+    }
+    .smallText {
+      font-size: 44px;
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,15 +2,27 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import DetailsPage from "./pages/DetailsPage.vue";
+import NotFoundPage from "./pages/404.vue";
 
 const routes = [
   { path: "/", component: HomePage },
-  { path: "/movie/:id", component: DetailsPage },
+  { name: "movie", path: "/movie/:id", component: DetailsPage },
+  { name: "tv", path: "/tv/:id", component: DetailsPage },
+  {
+    path: "/:notFound",
+    component: NotFoundPage,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to) {
+    return {
+      el: "body",
+      behavior: "smooth",
+    };
+  },
 });
 
 export default router;
